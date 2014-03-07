8db1e9c77898ca0a79674f61de1fbb18c359aa1c
bryan kennedy <bkennedy@smm.org>
2014-03-07 17:07:29 -0600

    Adding some initial date picker buttons
    These simple buttons make it a bit easier to pick the date of the
    visualization that you want. This is only the skeleton of the
    functionality and the buttons don't actually work yet.
    
    Using the momment library to handle date strings. I can't abide JS's
    ploopy date formatting.

--- public/libs/smm/smm.js
+++ public/libs/smm/smm.js
@@ -1,3 +1,9 @@
 (function() {
+    // Hide the full controls
     $('#earth').hide();
+
+    // Add today's date to the 'today' button
+    var now = moment();
+    $('#show-back-1').html('Yesterday<br />' + moment().subtract('days', 1).format('MMMM D'));
+    $('#show-today').html('Today<br />' + moment().format('MMMM D'));
 })();
