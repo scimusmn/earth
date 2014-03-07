799d9aaff5a0a9f33d5fa107d52ed7969bffa1ea
bryan kennedy <bkennedy@smm.org>
2014-03-07 17:48:23 -0600

    Basic date switching functionality
    This is very brittle but this sends the document back and forth between
    the date fields for yesterday and today.

--- public/libs/smm/smm.js
+++ public/libs/smm/smm.js
@@ -4,6 +4,25 @@
 
     // Add today's date to the 'today' button
     var now = moment();
-    $('#show-back-1').html('Yesterday<br />' + moment().subtract('days', 1).format('MMMM D'));
-    $('#show-today').html('Today<br />' + moment().format('MMMM D'));
+    var daysBack1 = moment().subtract('days', 1);
+    var daysBack2 = moment().subtract('days', 2);
+
+    //$('#show-back-2').html('Two days ago<br />' + daysBack2.format('MMMM D'));
+    $('#show-back-1').html('Yesterday<br />' + daysBack1.format('MMMM D'));
+    $('#show-today').html('Today<br />' + now.format('MMMM D'));
+
+    $('#show-back-1').click(function(){
+        //window.location=$(this).find("a").attr("href");
+        window.location="http://127.0.0.1:8090/#" +
+            daysBack1.format('YYYY/MM/DD') +
+            "/0000Z/wind/surface/level/equirectangular=-99.43,35.45,2048";
+        return false;
+    });
+    $('#show-today').click(function(){
+        //window.location=$(this).find("a").attr("href");
+        window.location="http://127.0.0.1:8090/#" +
+            now.format('YYYY/MM/DD') +
+            "/0000Z/wind/surface/level/equirectangular=-99.43,35.45,2048";
+        return false;
+    });
 })();
