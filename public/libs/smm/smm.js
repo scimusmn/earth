ae5862a36e8fa34f6191beb6d1aeed95fbd47c1c
bryan kennedy <bkennedy@smm.org>
2014-03-10 15:02:27 -0500

    Adding headline, custom fonts, and extra days
    Adding html for a headline graphic
    
    Adding some references to custom fonts specific to the Conner Prairie
    exhibit. I won't be including the fonts because I don't have the license
    to redistribute them.
    
    Added a button for another day backwards.

--- public/libs/smm/smm.js
+++ public/libs/smm/smm.js
@@ -7,10 +7,17 @@
     var daysBack1 = moment().subtract('days', 1);
     var daysBack2 = moment().subtract('days', 2);
 
-    //$('#show-back-2').html('Two days ago<br />' + daysBack2.format('MMMM D'));
+    $('#show-back-2').html('Two days ago<br />' + daysBack2.format('MMMM D'));
     $('#show-back-1').html('Yesterday<br />' + daysBack1.format('MMMM D'));
     $('#show-today').html('Today<br />' + now.format('MMMM D'));
 
+    $('#show-back-2').click(function(){
+        //window.location=$(this).find("a").attr("href");
+        window.location="http://127.0.0.1:8090/#" +
+            daysBack2.format('YYYY/MM/DD') +
+            "/0000Z/wind/surface/level/equirectangular=-86.02,39.98,2048";
+        return false;
+    });
     $('#show-back-1').click(function(){
         //window.location=$(this).find("a").attr("href");
         window.location="http://127.0.0.1:8090/#" +
