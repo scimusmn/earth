92c6f4a6b35a6ca24f8961834f2403c3b55005ad
bryan kennedy <bkennedy@smm.org>
2014-03-11 11:11:36 -0500

    Upadting custom date selectors
    Adding some key events and improving the DRY aspects of the code.

--- public/libs/smm/smm.js
+++ public/libs/smm/smm.js
@@ -2,34 +2,63 @@
     // Hide the full controls
     $('#earth').hide();
 
-    // Add today's date to the 'today' button
+    /*
+     * This isn't directly centered on Conner Prairie, but it displays most
+     * of the US and the Gulf of Mexico, which is important for Katrina
+     */
+    var homeCoords = '-93.40,35.35,2048';
+
+    /*
+     * Add dates to the event buttons
+     */
     var now = moment();
     var daysBack1 = moment().subtract('days', 1);
-    var daysBack2 = moment().subtract('days', 2);
-
-    $('#show-back-2').html('Two days ago<br />' + daysBack2.format('MMMM D'));
-    $('#show-back-1').html('Yesterday<br />' + daysBack1.format('MMMM D'));
-    $('#show-today').html('Today<br />' + now.format('MMMM D'));
-
-    $('#show-back-2').click(function(){
+    var testOne = moment('September 29, 2092');
+    var testTwo = moment('September 28, 2092');
+    var hurricaneSandy = moment('October 29, 2012');
+    var indianaTornados = moment('March 2, 2012');
+    var hurricaneKatrina = moment('August 29, 2005');
+    // Longest date strings for design testing
+    // $('#show-today').html('Today<br /><span class="date-string">' + testOne.format('MMMM D') + '</span>');
+    // $('#show-back-1').html('Yesterday<br /><span class="date-string">' + testTwo.format('MMMM D') + '</span>');
+    $('#show-today').html('Today<br /><span class="date-string">' + now.format('MMMM D') + '</span>');
+    $('#show-back-1').html('Yesterday<br /><span class="date-string">' + daysBack1.format('MMMM D') + '</span>');
+    $('#show-sandy').html('Hurricane Sandy<br /><span class="date-string">' + hurricaneSandy.format('MMMM D, YYYY') + '</span>');
+    $('#show-indiana').html('Indiana Tornados<br /><span class="date-string">' + indianaTornados.format('MMMM D, YYYY') + '</span>');
+    $('#show-katrina').html('Hurricane Katrina<br /><span class="date-string">' + hurricaneKatrina.format('MMMM D, YYYY') + '</span>');
+    $('#show-today').click(function(){
         //window.location=$(this).find("a").attr("href");
         window.location="http://127.0.0.1:8090/#" +
-            daysBack2.format('YYYY/MM/DD') +
-            "/0000Z/wind/surface/level/equirectangular=-86.02,39.98,2048";
+            now.format('YYYY/MM/DD') +
+            "/0000Z/wind/surface/level/equirectangular=" + homeCoords;
         return false;
     });
     $('#show-back-1').click(function(){
         //window.location=$(this).find("a").attr("href");
         window.location="http://127.0.0.1:8090/#" +
             daysBack1.format('YYYY/MM/DD') +
-            "/0000Z/wind/surface/level/equirectangular=-86.02,39.98,2048";
+            "/0000Z/wind/surface/level/equirectangular=" + homeCoords;
         return false;
     });
-    $('#show-today').click(function(){
+    $('#show-sandy').click(function(){
         //window.location=$(this).find("a").attr("href");
         window.location="http://127.0.0.1:8090/#" +
-            now.format('YYYY/MM/DD') +
-            "/0000Z/wind/surface/level/equirectangular=-86.02,39.98,2048";
+            hurricaneSandy.format('YYYY/MM/DD') +
+            "/0000Z/wind/surface/level/equirectangular=" + homeCoords;
+        return false;
+    });
+    $('#show-indiana').click(function(){
+        //window.location=$(this).find("a").attr("href");
+        window.location="http://127.0.0.1:8090/#" +
+            indianaTornados.format('YYYY/MM/DD') +
+            "/0000Z/wind/surface/level/equirectangular=" + homeCoords;
+        return false;
+    });
+    $('#show-katrina').click(function(){
+        //window.location=$(this).find("a").attr("href");
+        window.location="http://127.0.0.1:8090/#" +
+            hurricaneKatrina.format('YYYY/MM/DD') +
+            "/0000Z/wind/surface/level/equirectangular=" + homeCoords;
         return false;
     });
 })();
